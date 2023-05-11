899f33ca83aa5ad6d0b1bfd711b0fec73077bce1
Rociogm27 <rociogoomez27@uma.es>
2023-05-11 10:39:44 +0200

    cambios entre headers

--- src/registro/registro.js
+++ src/registro/registro.js
@@ -57,7 +57,7 @@ const CompRegistro = () =>{
                         <div className='row'>
                             <div className='col'>
                                 <div className='card' style={{padding: '20px', backgroundColor:'rgba(255, 255, 255, 0.7)'}}>
-                                    <h1 style={{fontSize: '2.5rem', color: '#333'}}>VSR, compartir es ahorrar</h1>
+                                    <h1 style={{fontSize: '2.5rem', color: '#333'}}>¿Qué es VSR?</h1>
                                     <div className='d-flex text-start'>
                                         <p style={{fontSize: '1.25rem', color: '#555'}} tabindex="0">VSR es una aplicación diseñada para crear almacenes virtuales compartidos entre familiares, amigos y conocidos, permitiéndoles gestionar inventarios de distintos ámbitos y sus respectivos objetos. Con VSR, los usuarios tienen la capacidad de cambiar objetos, editar su descripción, condiciones de uso y ubicación reciente, y también pueden reservar los objetos por un tiempo determinado. En resumen, VSR es una solución de gestión de inventario en línea altamente personalizable y colaborativa.</p>
                                     </div>
@@ -70,6 +70,7 @@ const CompRegistro = () =>{
                                     <img alt='foto almacen' src={fotoAlmacen} style={{width: '100%', height:'100%', objectFit:'contain'}} className="card-img-top img-fluid"></img>
                                     <div className='card-title mt-2'>
                                         <h2>Almacenes</h2>
+                                        <span class="hidden">Foto almacen</span>
                                     </div>
                                 </div>
                             </div>
@@ -78,6 +79,7 @@ const CompRegistro = () =>{
                                     <img alt='foto objeto' src={fotoObjeto} style={{width: '100%', height:'100%', objectFit:'contain'}} className="card-img-top img-fluid"></img>
                                     <div className='card-title mt-2'>
                                         <h2>Objetos</h2>
+                                        <span class="hidden">Foto objeto</span>
                                     </div>
                                 </div>
                             </div>
@@ -86,6 +88,7 @@ const CompRegistro = () =>{
                                     <img alt='foto usuario' src={fotoUsuario} style={{width: '100%', height:'100%', objectFit:'contain'}} className="card-img-top img-fluid"></img>
                                     <div className='card-title mt-2'>
                                         <h2>Usuarios</h2>
+                                        <span class="hidden">Foto usuario</span>
                                     </div>
                                 </div>
                             </div>
